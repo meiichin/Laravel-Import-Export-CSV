8489d4837d2777a2955d65578d260b64bfaa829d
meiichin <meiichan3017@gmail.com>
2019-03-21 22:54:55 +0700

    [Added] route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('items', 'ItemController@index');
+Route::post('items/import', 'ItemController@import');
+Route::get('items/export', 'ItemController@export');
